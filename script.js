c04ad32dc40c35083fcec260f74158ec8db29586
HromekFrantisek <hromekfrantisek2@gmail.com>
2020-09-21 09:33:34 +0200

    Functional fix

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ var month = today.getMonth();
 var year = today.getFullYear();
 var fullDate = day + "/" + month + "/" + year;
 
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return [weekNo];
+}
+
 function isEven() {
     var isEvenText = document.getElementById("even");
-    if (month % 2 == 1) {
+    if (getWeekNumber(today) % 2 == 1) {
         isEvenText.innerHTML = ("Tento týden je lichý").toUpperCase();
     }
     else {
